db2d3f416d94659ea810196901a48f924f50ff17
amalcaraz <amalcaraz89@gmail.com>
2023-09-21 09:31:27 +0200

    update lerna version using npm workspaces now

--- packages/aleph-messages/src/constants.ts
+++ packages/aleph-messages/src/constants.ts
@@ -1,88 +1,88 @@
-import { PublicKey } from "@solana/web3.js"
-import { AlephSolanaContract } from "./types"
+import { PublicKey } from '@solana/web3.js'
+import { AlephSolanaContract } from './types'
 
 export const SOLANA_MESSAGES_PROGRAM_ID =
   'ALepH1n9jxScbz45aZhBYVa35zxBNbKSvL6rWQpb4snc'
 export const SOLANA_MESSAGES_PROGRAM_ID_PK = new PublicKey(
-    SOLANA_MESSAGES_PROGRAM_ID,
+  SOLANA_MESSAGES_PROGRAM_ID,
 )
 
 export const SOLANA_MESSAGES_PROGRAM_IDL: AlephSolanaContract = {
-  "version": "0.1.0",
-  "name": "aleph_solana_contract",
-  "instructions": [
+  version: '0.1.0',
+  name: 'aleph_solana_contract',
+  instructions: [
     {
-      "name": "doEmit",
-      "accounts": [
+      name: 'doEmit',
+      accounts: [
         {
-          "name": "sender",
-          "isMut": true,
-          "isSigner": true
-        }
+          name: 'sender',
+          isMut: true,
+          isSigner: true,
+        },
       ],
-      "args": [
+      args: [
         {
-          "name": "message",
-          "type": "string"
-        }
-      ]
+          name: 'message',
+          type: 'string',
+        },
+      ],
     },
     {
-      "name": "doMessage",
-      "accounts": [
+      name: 'doMessage',
+      accounts: [
         {
-          "name": "sender",
-          "isMut": true,
-          "isSigner": true
-        }
+          name: 'sender',
+          isMut: true,
+          isSigner: true,
+        },
       ],
-      "args": [
+      args: [
         {
-          "name": "msgtype",
-          "type": "string"
+          name: 'msgtype',
+          type: 'string',
         },
         {
-          "name": "msgcontent",
-          "type": "string"
-        }
-      ]
-    }
+          name: 'msgcontent',
+          type: 'string',
+        },
+      ],
+    },
   ],
-  "events": [
+  events: [
     {
-      "name": "SyncEvent",
-      "fields": [
+      name: 'SyncEvent',
+      fields: [
         {
-          "name": "address",
-          "type": "publicKey",
-          "index": false
+          name: 'address',
+          type: 'publicKey',
+          index: false,
         },
         {
-          "name": "message",
-          "type": "string",
-          "index": false
-        }
-      ]
+          name: 'message',
+          type: 'string',
+          index: false,
+        },
+      ],
     },
     {
-      "name": "MessageEvent",
-      "fields": [
+      name: 'MessageEvent',
+      fields: [
         {
-          "name": "address",
-          "type": "publicKey",
-          "index": false
+          name: 'address',
+          type: 'publicKey',
+          index: false,
         },
         {
-          "name": "msgtype",
-          "type": "string",
-          "index": false
+          name: 'msgtype',
+          type: 'string',
+          index: false,
         },
         {
-          "name": "msgcontent",
-          "type": "string",
-          "index": false
-        }
-      ]
-    }
-  ]
+          name: 'msgcontent',
+          type: 'string',
+          index: false,
+        },
+      ],
+    },
+  ],
 }
