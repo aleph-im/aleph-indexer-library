1213091ef8d17318750a7d4cf89a066c07e03db1
Riki <ricardocr987@gmail.com>
2024-04-15 11:07:46 +0200

    fix query type, add owner on snapshot

--- packages/aleph-holders/src/api/types.ts
+++ packages/aleph-holders/src/api/types.ts
@@ -40,11 +40,23 @@ export const Balance = new GraphQLObjectType({
 
 export const BalanceList = new GraphQLList(Balance)
 
+export const SolanaBalance = new GraphQLObjectType({
+  name: 'SolanaBalance',
+  fields: {
+    account: { type: new GraphQLNonNull(GraphQLString) },
+    owner: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLBigNumber) },
+    balanceNum: { type: GraphQLFloat },
+  },
+})
+
+export const SolanaBalanceList = new GraphQLList(SolanaBalance)
+
 export const Snapshot = new GraphQLObjectType({
   name: 'Snapshot',
   fields: {
     timestamp: { type: new GraphQLNonNull(GraphQLString) },
-    balances: { type: new GraphQLNonNull(BalanceList) },
+    balances: { type: new GraphQLNonNull(SolanaBalanceList) },
   },
 })
 
