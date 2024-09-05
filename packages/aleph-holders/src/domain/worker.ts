0627243ac8a5d1614251fcc0b77915ef787ddcb9
amalcaraz <amalcaraz89@gmail.com>
2024-09-05 22:25:42 +0200

    fix: balance sorting

--- packages/aleph-holders/src/domain/worker.ts
+++ packages/aleph-holders/src/domain/worker.ts
@@ -355,7 +355,7 @@ export default class WorkerDomain
       .filter(({ balanceBN }) => !balanceBN.isZero())
       .sort(
         ({ balanceBN: a }, { balanceBN: b }) =>
-          (args.reverse ? -1 : 1) * (a.lt(b) ? -1 : a.gt(b) ? 1 : 0),
+          (reverse ? -1 : 1) * (a.lt(b) ? -1 : a.gt(b) ? 1 : 0),
       )
   }
 
