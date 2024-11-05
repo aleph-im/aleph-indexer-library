5a2952bd5aa5546545eee6fda9004d071affff97
amalcaraz <amalcaraz89@gmail.com>
2024-11-05 14:20:24 +0100

    fix: index main aleph collection

--- packages/aleph-vouchers/src/utils/constants.ts
+++ packages/aleph-vouchers/src/utils/constants.ts
@@ -6,6 +6,6 @@ export const METAPLEX_CORE_PROGRAM_ID =
   'CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d'
 
 export const blockchainTokenContract: Record<BlockchainId, string> = {
-  // [BlockchainChain.Solana]: '3V1UtauHS2F31civmDaT5MFYkntSjX82LsDcBKCZf5iJ', // Mainnet
-  [BlockchainChain.Solana]: 'E5g1xaobkaVpAgJQKMVQ3TwB3YuZuU52TeDHx5RHJdrD', // MAinnet TEST
+  [BlockchainChain.Solana]: '3V1UtauHS2F31civmDaT5MFYkntSjX82LsDcBKCZf5iJ', // Mainnet
+  // [BlockchainChain.Solana]: 'E5g1xaobkaVpAgJQKMVQ3TwB3YuZuU52TeDHx5RHJdrD', // MAinnet TEST
 }
